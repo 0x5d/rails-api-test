952d9fa8c876f1cf764358de4e0c90b7aefd3e50
Juan Castillo <castillobgr@gmail.com>
2015-06-26 08:26:35 -0500

    Removed comments.

--- app/api/task_flex/root.rb
+++ app/api/task_flex/root.rb
@@ -5,13 +5,6 @@ module TaskFlex
     # Separate the api into smaller
     # modules like this
     mount TaskFlex::V1::Root => '/v1'
-
-    # # Adds the swagger documentation to your
-    # # api. You only need this once, not in
-    # # every sub module
-    # add_swagger_documentation(
-    #   base_path: "/api/v1",
-    #   hide_documentation_path: true
-    # )
+    
   end
 end
